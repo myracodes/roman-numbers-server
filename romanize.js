775f36c5becb1695d627daad7ffff59fc92ba2e4
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-03-28 14:29:53 +0200

    Create routes to use (de)romanize functions

---
+++ romanize.js
@@ -0,0 +1,22 @@
+module.exports = { romanize: (num) => {
+    if (!+num) return false;
+    var digits = String(+num).split('');
+    var key = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM',
+               '','X','XX','XXX','XL','L','LX','LXX','LXXX','XC',
+               '','I','II','III','IV','V','VI','VII','VIII','IX'];
+    var roman = '', i = 3;
+    while (i--) roman = (key[+digits.pop() + (i * 10)] || '') + roman;
+    return Array(+digits.join('') + 1).join('M') + roman;
+},
+ deromanize: (str) => {
+    var str = str.toUpperCase();
+    var validator = /^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/;
+    var token = /[MDLV]|C[MD]?|X[CL]?|I[XV]?/g;
+    var key = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    var num = 0, m;
+    if (!(str && validator.test(str))) return false;
+    while (m = token.exec(str)) num += key[m[0]];
+    return num;
+}}
+
+// module.exports = { deromanize, romanize };
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const { romanize, deromanize } = require('./romanize'); 
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+// soit faire une route POST pour récupérer le contenu depuis le body de la requête
+// soit récupérer l'info depuis l'URL (GET) : route 
+
+app.get(`/romanize/:number`, (req, res) => {
+  res.send(romanize(req.params.number));
+})
+app.get(`/deromanize/:roman`, (req, res) => {
+  res.send(deromanize(req.params.roman));
 })
 
 app.listen(port, () => {
